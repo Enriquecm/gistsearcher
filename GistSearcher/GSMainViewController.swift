97dcbbba109282dd51197058fdaa15e46a8c637a
Enrique Melgarejo <enriquecmelgarejo@hotmail.com>
2017-04-21 15:43:36 -0300

    DEV Created project and first app flow; - Added icons and background images;

---
+++ GistSearcher/GSMainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  GSMainViewController.swift
+//  GistSearcher
+//
+//  Created by Enrique Melgarejo on 17/04/17.
+//  Copyright © 2017 Choynowski. All rights reserved.
+//
+
+import UIKit
+
+class GSMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func buttonSearchPressed(_ sender: UIButton) {
+        
+    }
+
+    @IBAction func buttonQRCodePressed(_ sender: UIButton) {
+        
+    }
+}
+
