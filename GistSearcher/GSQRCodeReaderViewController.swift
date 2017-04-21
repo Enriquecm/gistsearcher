2d8f7d175862df4fa237a0c8cf74ec245ad1587c
Enrique Melgarejo <enriquecmelgarejo@hotmail.com>
2017-04-21 15:58:39 -0300

    DEV Created View Controllers and update storyboard

---
+++ GistSearcher/GSQRCodeReaderViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GSQRCodeReaderViewController.swift
+//  GistSearcher
+//
+//  Created by Enrique Melgarejo on 21/04/17.
+//  Copyright © 2017 Choynowski. All rights reserved.
+//
+
+import UIKit
+
+class GSQRCodeReaderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
