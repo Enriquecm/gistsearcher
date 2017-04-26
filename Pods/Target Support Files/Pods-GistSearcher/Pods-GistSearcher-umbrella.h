b989896cb527d58984144b1b1803754d9aa7ded5
Enrique Melgarejo <enriquecmelgarejo@hotmail.com>
2017-04-26 20:31:17 -0300

    DEV Added cocoa pods with Alamofire framework

---
+++ Pods/Target Support Files/Pods-GistSearcher/Pods-GistSearcher-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_GistSearcherVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GistSearcherVersionString[];
+
---
+++ Pods/Target Support Files/Pods-GistSearcherTests/Pods-GistSearcherTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GistSearcherTests : NSObject
+@end
+@implementation PodsDummy_Pods_GistSearcherTests
+@end
